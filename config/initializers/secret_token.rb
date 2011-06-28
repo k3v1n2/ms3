9ec3f9b68d09692c5f31630d389f1f89a101727b
Kevin Walters <kevin.walters2@gmail.com>
2011-06-28 19:52:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ms3::Application.config.secret_token = 'b3598d47841283dda09e829acb2f2f9d0d65b1e18b0e169bea64278206ca6819dbb8202d3cf9e9a7dabb922c192d980ed9b8e2f9068e0ed4497c31c7eed5555a'
